fc199750b63ebbe9405d9dbb0685ec5603cace81
evgkit <kitaev.ea@gmail.com>
2015-12-28 21:56:15 +0300

    Add blog route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    posts   = require('./mock/posts.json');
 
 var app = express();
 
 app.get('/', function(req, res) {
     res.send('Wow! It\'s get working!');
+});
+
+app.get('/blog', function(req, res) {
+    res.send(posts);
 });
 
 app.listen(3000, function() {
