971f745b767f6db4adb8206a7e98289da2bb6012
evgkit <kitaev.ea@gmail.com>
2016-01-01 00:34:39 +0300

    Add few views

--- src/app.js
+++ src/app.js
@@ -4,22 +4,23 @@ var express = require('express'),
     posts   = require('./mock/posts.json');
 
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res) {
-    res.send('<h1>Wow! Express JS is working!</h1>');
+    res.render('index');
 });
 
 app.get('/blog/:title?', function(req, res) {
     var title = req.params.title,
-        post = posts[title];
+        post = posts[title] || {};
 
     if (undefined !== title) {
-        res.send(post);
+        res.render('post', {post: post});
     } else {
         res.status(503);
         res.send('<p>Halt! Page is under construction</p>');
     }
-
 });
 
 app.listen(3000, function() {
